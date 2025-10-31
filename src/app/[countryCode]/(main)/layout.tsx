283440f547b339fd4fce952c5778ed87dada114f
Hans Krogh Thomsen <hkt@sublime-it.com>
2025-10-31 10:52:04 +0100

    feat: home page

---
+++ src/app/[countryCode]/(main)/layout.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/features/navbar/navbar";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ThorApolloProvider>
           <main className="main-root">
-            <Navbar />
             {children}
           </main>
         </ThorApolloProvider>
