18032392976163b6334140ac958e54cffa9d5515
Hans Krogh Thomsen <hkt@sublime-it.com>
2025-11-05 21:55:35 +0100

    fix: improve payment processing

--- src/app/[countryCode]/(checkout)/checkout/page.tsx
+++ src/app/[countryCode]/(checkout)/checkout/page.tsx
@@ -1,15 +1,20 @@
 import { SkeletonBox } from "@/components/skeleton-box/skeleton-box";
 import { CACHE_TAGS } from "@/constants";
+import Stripe from "stripe";
+
 import { getCartIdFromCookies } from "@/features/cart/utils";
 import CheckoutContainer from "@/features/checkout/checkout-container/checkout-container";
-import CheckoutMain, { CheckoutMainSkeleton } from "@/features/checkout/checkout-main/checkout-main";
+import CheckoutMain, {
+  CheckoutMainSkeleton,
+} from "@/features/checkout/checkout-main/checkout-main";
 import PaymentWrapper from "@/features/checkout/checkout-main/checkout-payment/payment-wrapper";
 import CheckoutSummary, {
   CheckoutSummarySkeleton,
 } from "@/features/checkout/checkout-summary/checkout-summary";
-import PaymentProcessingScreen from "@/features/checkout/payment-processing-screen/payment-processing-screen";
+import PaymentProcessingScreen, { PaymentProcessingScreenSkeleton } from "@/features/checkout/payment-processing-screen/payment-processing-screen";
 import { CHECKOUT_CART_DETAILS_QUERY } from "@/features/checkout/queries";
-import { PreloadQuery } from "@/lib/thor/apollo-client";
+import { getClient, PreloadQuery } from "@/lib/thor/apollo-client";
+import { loadStripe } from "@stripe/stripe-js";
 import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
@@ -22,8 +27,11 @@ export default async function CheckoutPage({
     processing_payment,
     redirect_status,
     checkout_id,
+    payment_intent_client_secret,
   }: {
     processing_payment?: string;
+    payment_intent?: string;
+    payment_intent_client_secret?: string;
     redirect_status?: "failed" | "success" | string[];
     checkout_id?: string;
   } = await searchParams;
@@ -36,10 +44,41 @@ export default async function CheckoutPage({
   if (redirect_status === "failed" || Array.isArray(redirect_status)) {
     redirect(`/${country}/checkout?checkout_id=${checkout_id}`);
   }
+
   if (processing_payment === "true") {
-    return <PaymentProcessingScreen cartId={checkout_id} />;
+    const tt = await getClient().query({
+      query: CHECKOUT_CART_DETAILS_QUERY,
+      variables: { id: checkout_id },
+      context: {
+        fetchOptions: {
+          tags: [CACHE_TAGS.cart],
+        },
+      },
+    });
+    const publishableKey =
+      tt.data?.cart?.paymentSession?.paymentGateway.publishableKey;
+    if (!publishableKey || !payment_intent_client_secret) {
+      return <div> no publishableKey or payment_intent_client_secret </div>;
+    }
+
+    return (
+      <PreloadQuery
+        query={CHECKOUT_CART_DETAILS_QUERY}
+        variables={{ id: checkout_id }}
+        context={{
+          fetchOptions: {
+            tags: [CACHE_TAGS.cart],
+          },
+        }}
+      >
+        {(queryRef) => (
+          <Suspense fallback={<PaymentProcessingScreenSkeleton />}>
+            <PaymentProcessingScreen queryRef={queryRef} />
+          </Suspense>
+        )}
+      </PreloadQuery>
+    );
   }
-  
 
   return (
     <Suspense
@@ -71,36 +110,3 @@ export default async function CheckoutPage({
     </Suspense>
   );
 }
-
-//  <CheckoutContainer
-//           mainArea={
-//             <Suspense fallback={<SkeletonBox width={200} height={200} />}>
-//               <PreloadQuery
-//                 query={CHECKOUT_CART_DETAILS_QUERY}
-//                 variables={{ id: checkout_id }}
-//                 context={{
-//                   fetchOptions: {
-//                     tags: [CACHE_TAGS.cart],
-//                   },
-//                 }}
-//               >
-//                 {(queryRef) => <CheckoutMain queryRef={queryRef} />}
-//               </PreloadQuery>
-//             </Suspense>
-//           }
-//           summaryArea={
-//             <Suspense fallback={<CheckoutSummarySkeleton />}>
-//               <PreloadQuery
-//                 query={CHECKOUT_CART_DETAILS_QUERY}
-//                 variables={{ id: checkout_id }}
-//                 context={{
-//                   fetchOptions: {
-//                     tags: [CACHE_TAGS.cart],
-//                   },
-//                 }}
-//               >
-//                 {(queryRef) => <CheckoutSummary queryRef={queryRef} />}
-//               </PreloadQuery>
-//             </Suspense>
-//           }
-//         />
--- src/features/checkout/payment-processing-screen/payment-processing-screen.tsx
+++ src/features/checkout/payment-processing-screen/payment-processing-screen.tsx
@@ -1,25 +1,69 @@
 "use client";
-import { useEffect } from "react";
+import { use, useEffect } from "react";
 import s from "./payment-processing-screen.module.css";
 
+import { CheckoutCartDetailsQuery } from "@/__generated__/thor/graphql";
 import Spinner from "@/components/spinner/spinner";
+import { QueryRef, useReadQuery } from "@apollo/client/react";
+import { loadStripe } from "@stripe/stripe-js";
 import { placeOrder } from "../actions";
 
 interface Props {
-  cartId: string;
+  queryRef: QueryRef<CheckoutCartDetailsQuery>;
 }
 
-export default function PaymentProcessingScreen({ cartId }: Props) {
+export default function PaymentProcessingScreen({ queryRef }: Props) {
+  const { data } = useReadQuery(queryRef);
+
+  if (!data.cart) {
+    throw new Error("No cart found");
+  }
+
+  const paymentSession = data?.cart?.paymentSession;
+
+  if (!paymentSession) {
+    throw new Error("No payment session found");
+  }
+
+  const publishableKey = paymentSession.paymentGateway.publishableKey;
+  const clientSecret = paymentSession.clientSecret;
+
+  if (!publishableKey) {
+    throw new Error("No publishable key found");
+  }
+
   const onPaymentComplete = async () => {
-    await placeOrder({ cartId }).catch(() => {
-      //TODO: error handling
-    });
+    const stripe = await loadStripe(publishableKey);
+    if (!stripe) {
+      throw new Error("Stripe failed to load");
+    }
+    const { paymentIntent, error } = await stripe.retrievePaymentIntent(
+      clientSecret
+    );
+
+    if (error) {
+      throw new Error(`PaymentIntent retrieval failed: ${error.message}`);
+    }
+
+    //in the purpose of this demo, we will not handle payment statuses like processing, requires_action, etc.
+    if (
+      paymentIntent.status === "succeeded" ||
+      paymentIntent.status === "requires_capture" 
+    ) {
+      await placeOrder({ cartId: data.cart!.id });
+    }
   };
 
   useEffect(() => {
     void onPaymentComplete();
   }, []);
 
+  return (
+    <PaymentProcessingScreenSkeleton />
+  );
+}
+
+export const PaymentProcessingScreenSkeleton = () => {
   return (
     <div className={s.root}>
       <div className={s.container}>
